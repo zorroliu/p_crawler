7a016bee10887c7a2ddf278b4bc8824508d35def
FunnyPuppet <88126376+FunnyPuppet@users.noreply.github.com>
2023-08-21 19:51:24 +0800

    2023-08-21 update

--- base/exception.py
+++ base/exception.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8  -*-
 
 class BusinessError(Exception):
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
 
     def __str__(self):
-        return repr(self.value)
+        return repr(self.message)
