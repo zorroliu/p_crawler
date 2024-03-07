d173d64e4669d7269ed1eb8c2044f603f17274d1
FunnyPuppet <88126376+FunnyPuppet@users.noreply.github.com>
2024-03-07 17:12:24 +0800

    2024-03-07 update

--- base/logger.py
+++ base/logger.py
@@ -19,12 +19,8 @@ class CustLogger:
         self._logger = logger.bind(name=name)
         self._p_log_path(log_path)
 
-        log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{" \
-                     "name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-
-        self._logger.remove()
-        self._logger.add(self._log_path, rotation="00:00", format=log_format, encoding="utf-8")
-        self._logger.add(sys.stdout, format=log_format)
+        self._log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{" \
+                           "name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 
     def _p_log_path(self, log_path):
         if log_path:
@@ -34,6 +30,12 @@ class CustLogger:
             self._log_path = os.path.join(base_logs_path, self._bind_name, 'server.log')
 
     def log(self, message, level: LogLevel = LogLevel.INFO):
+        self._logger = logger.bind(name=self._bind_name)
+
+        self._logger.remove()
+        self._logger.add(self._log_path, rotation="00:00", format=self._log_format, encoding="utf-8")
+        self._logger.add(sys.stdout, format=self._log_format)
+
         getattr(self._logger, level.value)(message)
 
 
--- test/http_test.py
+++ test/http_test.py
@@ -1,9 +1,16 @@
 import unittest
+from base.logger import http_log, main_log, fg_log
 
 
 class HttpTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        http_log.log("1")
+        main_log.log("2")
+        fg_log.log("3")
+        http_log.log("4")
+        main_log.log("5")
+        fg_log.log("6")
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == '__main__':
